2ae461293a9176d0bb7617f25fb496423845a883
Krishanth0 <98431027+Krishanth0@users.noreply.github.com>
2024-02-15 20:12:37 +0100

    ++logo on navbar and ++ flex color tailwindcss

--- boulangerie-app-react/src/components/Navbar.jsx
+++ boulangerie-app-react/src/components/Navbar.jsx
@@ -1,25 +1,21 @@
-import React from 'react'
-import logo from'../assets/Cakeclub-logo.png'
+import React from 'react';
+import logo from'../assets/Cakeclub-logo.png';
 
 const Navbar = () => {
   return (
-  
-    <div className='w-screen z-10 bg bg-transparent'>
-          <div>
-          <img src="Cakeclub-logo.png'go" alt="/" className="flex justify-center items-center" />
-        </div>
-    <div className='text-black flex-between'>
-        <ul>
+    <div className='text-white  flex-between bg-red-300'>
+        <ul className='flex flex-row justify-around items-center font-bold'>
         <li><a href="#">Nos Produits</a></li>
         <li><a href="#">Nos Boutiques</a></li>
-        <li><a href="#">Pakeclub</a></li>
+        <div>
+          <img src={logo} alt="/" className="flex justify-center items-center" />
+        </div>
         <li><a href="#">Galerie</a></li>
         <li><a href="#">A propos</a></li>
         <li><a href="#">Contact</a></li>
-        <li><a href="#">Contact</a></li>
       </ul>
     </div>
-    </div>
+
   )
 }
 
