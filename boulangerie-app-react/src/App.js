717f7a14ae96233b9c139a7b91a08c0426f7e57f
Krishanth0 <98431027+Krishanth0@users.noreply.github.com>
2024-02-15 20:25:33 +0100

    ++ init on App.js : Header, About, Gallery, Contact, Product, Reviews, Shops.

--- boulangerie-app-react/src/App.js
+++ boulangerie-app-react/src/App.js
@@ -1,10 +1,24 @@
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Header from './components/Header';
+import About from  './components/About';
+import Contact from  './components/Contact';
+import Gallery from  './components/Gallery';
+import Reviews from  './components/Reviews';
+import Shops from  "./components/Shops";
+import Product from  "./components/Product";
 function App() {
   return (
     <>
-    <Navbar/> 
+    <Navbar/>
+    <Header/>
+    <About/>
+    <Gallery/>
+    <Contact/>
+    <Product/>
     <Footer/>
+    <Reviews/>
+    <Shops/>
     </>
   );
 }
