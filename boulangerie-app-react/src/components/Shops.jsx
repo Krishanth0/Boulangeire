7aa673c97cf75e4dc3963677a018b654948938e5
Krishanth0 <98431027+Krishanth0@users.noreply.github.com>
2024-02-16 09:53:51 +0100

    ++ajouts des boutique : Paris, Lile, Toulouse

--- boulangerie-app-react/src/components/Shops.jsx
+++ boulangerie-app-react/src/components/Shops.jsx
@@ -6,10 +6,26 @@ const Shops = () => {
     <div>
       <h2 className='text-black text-center text-4xl font-bold '>Nos Boutiques</h2>
     </div>
-    <div className='flex flex-row px-3 mt-5'>
-    <h2>Viennoiseries</h2>
-    <h2>Patisseries</h2>
-    <h2>pain</h2>
+    <div className='flex flex-row mt-5 justify-around text-center'>
+
+      <div>
+      <h2 className='font-bold text-3xl'>Paris</h2>
+      <p>52 Rue Jenner, 75013 Paris</p>
+      <p>7h00 - 20h00 </p>
+      </div>
+
+      <div>
+      <h2 className='font-bold text-3xl'>Toulouse</h2>
+      <p>4 Rue Suger, 31000 Toulouse</p>
+      <p>7h00 - 20h00 </p>
+      </div>
+
+      <div>
+      <h2 className='font-bold text-3xl'>Lile</h2>
+      <p>23 Avenue Duquesne, 59000 Lille</p>
+      <p>7h00 - 20h00 </p>
+      </div>
+
     </div>
     </div>
   )
