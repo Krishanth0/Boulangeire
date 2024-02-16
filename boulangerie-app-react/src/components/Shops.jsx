07ec66ee47a72596ddf858cf8993a6d0ff98c620
Krishanth0 <98431027+Krishanth0@users.noreply.github.com>
2024-02-16 16:16:10 +0100

    ++ image croissant dans shop.jsx

--- boulangerie-app-react/src/components/Shops.jsx
+++ boulangerie-app-react/src/components/Shops.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
+import React from 'react';
+import img from '../assets/croissant.png';
 
 const Shops = () => {
   return (
-    <div className=' w-full h-full bg-yellow-50'>
-    <div>
-      <h1 className='text-black text-center text-4xl font-bold '>Nos Boutiques</h1>
-    </div>
-    <div className='flex flex-row mt-5 justify-around text-center'>
-
-      <div>
-      <h2 className='font-bold text-3xl'>Paris</h2>
-      <p>52 Rue Jenner, 75013 Paris</p>
-      <p>7h00 - 20h00 </p>
+    <div className='w-full h-full bg-yellow-50 p-20 relative'>
+      <div className="absolute top-0 right-0 transform rotate-[47.95deg]">
+        <img src={img} alt="/" className="w-[145.83px] h-[97.22px]" />
       </div>
-
       <div>
-      <h2 className='font-bold text-3xl'>Toulouse</h2>
-      <p>4 Rue Suger, 31000 Toulouse</p>
-      <p>7h00 - 20h00 </p>
+        <h1 className='text-black text-center text-4xl font-bold'>Nos Boutiques</h1>
       </div>
+      <div className='flex flex-row mt-8 justify-around text-center'>
 
-      <div>
-      <h2 className='font-bold text-3xl'>Lile</h2>
-      <p>23 Avenue Duquesne, 59000 Lille</p>
-      <p>7h00 - 20h00 </p>
+        <div className='mb-8'>
+          <h2 className='font-bold text-3xl'>Paris</h2>
+          <p>52 Rue Jenner, 75013 Paris</p>
+          <p>7h00 - 20h00 </p>
+        </div>
+
+        <div className='mb-8'>
+          <h2 className='font-bold text-3xl'>Toulouse</h2>
+          <p>4 Rue Suger, 31000 Toulouse</p>
+          <p>7h00 - 20h00 </p>
+        </div>
+
+        <div className='mb-8'>
+          <h2 className='font-bold text-3xl'>Lille</h2>
+          <p>23 Avenue Duquesne, 59000 Lille</p>
+          <p>7h00 - 20h00 </p>
+        </div>
+
       </div>
-
     </div>
-    </div>
-  )
+  );
 }
 
-export default Shops
+export default Shops;
