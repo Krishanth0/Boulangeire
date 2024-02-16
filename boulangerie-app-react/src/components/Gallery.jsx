87c3984a45a17a3c085467597228a19c5e3d12a3
Krishanth0 <98431027+Krishanth0@users.noreply.github.com>
2024-02-16 15:13:48 +0100

    +- Les images de Gallery sont représenté en  grid

--- boulangerie-app-react/src/components/Gallery.jsx
+++ boulangerie-app-react/src/components/Gallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import image1 from '../assets/galerie.png';
 import image2 from '../assets/galerie2.png';
 import image3 from '../assets/galerie3.png';
@@ -7,30 +7,21 @@ import image5 from '../assets/galerie5.png';
 import image6 from '../assets/galerie6.png';
 import image7 from '../assets/galerie7.png';
 
-
-
 const Gallery = () => {
   return (
-    <div>
-
-    <div>
-      <h2 className='text-black text-center text-4xl font-bold '>Galerie</h2>
+    <div className="container mx-auto p-8">
+      <h2 className="text-black text-center text-4xl font-bold mb-4">Galerie</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <img src={image1} alt="/" className="w-full h-full object-cover" />
+        <img src={image2} alt="/" className="w-full h-full object-cover" />
+        <img src={image3} alt="/" className="w-full h-full object-cover" />
+        <img src={image4} alt="/" className="w-full h-full object-cover" />
+        <img src={image5} alt="/" className="w-full h-full object-cover" />
+        <img src={image6} alt="/" className="w-full h-full object-cover" />
+        <img src={image7} alt="/" className="w-full h-full object-cover" />
+      </div>
     </div>
-
-     <div >
-     <img src={image1} alt="/" className="w-full h-full" />
-     <img src={image2} alt="/" className="w-full h-full" />
-     <img src={image3} alt="/" className="w-full h-full" />
-     <img src={image4} alt="/" className="w-full h-full" />
-     <img src={image5} alt="/" className="w-full h-full" />
-     <img src={image6} alt="/" className="w-full h-full" />
-     <img src={image7} alt="/" className="w-full h-full" />
-     </div>
-
-     </div>
-
-
-  )
+  );
 }
 
-export default Gallery
+export default Gallery;
