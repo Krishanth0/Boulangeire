cf10cd72986c49eb641469d9eb544602cfbd821a
Krishanth0 <98431027+Krishanth0@users.noreply.github.com>
2024-02-19 09:27:34 +0100

    +- modification taille des textes et import images pour la page header.jsx

--- boulangerie-app-react/src/components/Header.jsx
+++ boulangerie-app-react/src/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import bg from '../assets/cake-home.png';
+import line from '../assets/whiteline.png';
+import arrow from '../assets/arrow.png';
 
 const Header = () => {
   return (
     <div className="relative">
       <img src={bg} alt="/" className="w-full h-full" />
 
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
-        <h1 className="text-4xl font-bold mb-2">Boulangerie & Patisserie</h1>
-        <h2 className="text-xl mb-4">PARIS-LILE-TOULOUSE</h2>
-        <button className="bg-transparent border-white border px-4 py-2 text-white">Contactez-Nous</button>
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-white font-semibold">
+        <h1 className="text-[110px] mb-2">Boulangerie & Patisserie</h1>
+        <h2 className="text-[32px] mb-4">PARIS • LILE • TOULOUSE</h2>
+        <button className="bg-transparent border-4 border-white px-4 py-2 text-white text-lg mt-[19rem]">Contactez-Nous</button>
+        <img src={arrow} alt="Arrow" className="absolute bottom-0 mb-8 animate-bounce" style={{ animationDuration: '1s', animationIterationCount: 'infinite' }}/>
       </div>
     </div>
   );
