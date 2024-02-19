84af376b3114b2353594bc56a034a5d2d87097a7
Krishanth0 <98431027+Krishanth0@users.noreply.github.com>
2024-02-19 10:58:53 +0100

    +- taille des textes, font et test pour le rendre responsive

--- boulangerie-app-react/src/components/Navbar.jsx
+++ boulangerie-app-react/src/components/Navbar.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import logo from'../assets/Cakeclub-logo.png';
+import bg from '../assets/cake-home.png';
 
 const Navbar = () => {
   return (
-    <div className='text-white  flex-between bg-red-300'>
-        <ul className='flex flex-row justify-around items-center font-bold'>
+    <div className='text-white text-xl flex-between bg-black'>
+    <div className='container mx-auto'>
+        <ul className='flex flex-row justify-around items-center font-semibold '>
         <li><a href="#">Nos Produits</a></li>
         <li><a href="#">Nos Boutiques</a></li>
         <div>
-          <img src={logo} alt="/" className="flex justify-center items-center" />
+          <img src={logo} alt="/" className="flex justify-center items-center flex-shrink-0" />
         </div>
         <li><a href="#">Galerie</a></li>
         <li><a href="#">A propos</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
+    </div>
     </div>
 
   )
