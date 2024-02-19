ad507d2c8a8e663ae5f4c40de6af0fad67d29c7a
Krishanth0 <98431027+Krishanth0@users.noreply.github.com>
2024-02-19 09:39:30 +0100

    ++ AJOUTS ANIMATION EN BAS DE PAGE ET AJOUT DES LINES AVANT ET APRES LE TEXTE DANS HEADER.JSX

--- boulangerie-app-react/src/components/Header.jsx
+++ boulangerie-app-react/src/components/Header.jsx
@@ -10,9 +10,13 @@ const Header = () => {
 
       <div className="absolute inset-0 flex flex-col items-center justify-center text-white font-semibold">
         <h1 className="text-[110px] mb-2">Boulangerie & Patisserie</h1>
-        <h2 className="text-[32px] mb-4">PARIS • LILE • TOULOUSE</h2>
+        <h2 className="text-[32px] mb-4 relative">
+          <img src={line} alt="Line" className="absolute top-1/2 left-[-160px] " />
+          PARIS • LILE • TOULOUSE
+          <img src={line} alt="Line" className="absolute top-1/2 right-[-160px] " />
+        </h2>
         <button className="bg-transparent border-4 border-white px-4 py-2 text-white text-lg mt-[19rem]">Contactez-Nous</button>
-        <img src={arrow} alt="Arrow" className="absolute bottom-0 mb-8 animate-bounce" style={{ animationDuration: '1s', animationIterationCount: 'infinite' }}/>
+        <img src={arrow} alt="Arrow" className="absolute bottom-0 mb-8 animate-bounce" style={{ animationDuration: '1s', animationIterationCount: 'infinite' }} />
       </div>
     </div>
   );
